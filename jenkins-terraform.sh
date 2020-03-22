7536dd5044f80a49c780c064940c72fbeb9af4e7
rahulgaitonde <gaitonderahul@gmail.com>
2020-03-22 11:29:15 -0400

    Update jenkins-terraform.sh

--- jenkins-terraform.sh
+++ jenkins-terraform.sh
@@ -13,4 +13,5 @@ echo "AMI ID: ${AMI_ID}"
 echo "writing amivar.tf and uploading it to s3"
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+pwd
 aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
